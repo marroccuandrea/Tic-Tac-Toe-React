421ca6c136981f580ee66b6df84335dc1ebc29a3
marroccuandrea <marroccuandrea@yahoo.it>
2024-11-28 15:55:33 +0100

    [add] state and event handler for edit button in Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  // Con questa espressione posso capire quando il nome del giocatore viene modificato, inizialmente sarà false, al click del pulsante diventerà true e il nome del pulsante cambierà, a seconda dello state.
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
   return (
     <li>
       <span className="player">
         <span className="player-name">{name}</span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Modifica</button>
+      <button onClick={handleEditClick}>Modifica</button>
     </li>
   );
 }
