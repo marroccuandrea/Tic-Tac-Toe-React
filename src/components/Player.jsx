a08cca409c94f923726300b35d7d4eecb56abb4a
marroccuandrea <marroccuandrea@yahoo.it>
2024-11-28 18:04:13 +0100

    Updated Player component to use initialName prop instead of name, and added input field to edit player name (Two Way Binding)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,33 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  // Questo state mi servirà per modificare effettivamente il value dell'input
+  const [playerName, setPlayerName] = useState(initialName);
   // Con questa espressione posso capire quando il nome del giocatore viene modificato, inizialmente sarà false, al click del pulsante diventerà true e il nome del pulsante cambierà, a seconda dello state.
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     // setIsEditing(!isEditing); // Toggle salva - modifica
-    setIsEditing((editing) => !editing); //Quando si usano valori vecchi (old) meglio usare una arrow function, React la userà senza problemi, al contrario della prima.
+    setIsEditing((editing) => !editing); //Quando si usano valori vecchi (old state) meglio usare una arrow function, React la aggiornerà lo state senza problemi, al contrario della prima.
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+    // Questa funzione serve per prendere i valori all'interno del campo input e salvarli poi nella variabile
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>
