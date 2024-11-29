537c3cb3ab4213ccd87645952f80f5ad573b3a37
marroccuandrea <marroccuandrea@yahoo.it>
2024-11-29 17:59:36 +0100

    Pass game turns to Log component and render turn history

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
         </ol>
         <Gameboard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
